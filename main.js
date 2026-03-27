0858b7f572cb82d7a886a40ae87bbf8f0daad5c2
jkaps9 <jkaps9@users.noreply.github.com>
2026-03-27 19:40:11 +0000

    deploy: 7029012691057bf737856fce99332e1ac8d96695

--- main.js
+++ main.js
@@ -4,16 +4,26 @@ const mainNav = document.querySelector(".main-nav");
 
 if (navToggle && mainNav) {
   navToggle.addEventListener("click", () => {
+    const isExpanded = navToggle.getAttribute("aria-expanded") === true;
+    navToggle.setAttribute("aria-expanded", String(!isExpanded));
+    if (isExpanded) {
+      navToggle.setAttribute("hidden", "");
+    } else {
+      navToggle.removeAttribute("hidden");
+    }
     mainNav.classList.toggle("visible");
   });
 }
 
 const directorCards = document.querySelectorAll(".director-card");
 
-if (directorCards) {
+if (directorCards.length > 0) {
   directorCards.forEach((card) => {
-    card.querySelector(".director-card__btn").addEventListener("click", () => {
-      card.classList.toggle("flipped");
-    });
+    const btn = card.querySelector(".director-card__btn");
+    if (btn) {
+      btn.addEventListener("click", () => {
+        card.classList.toggle("flipped");
+      });
+    }
   });
 }
